ecf7ba639a6e3dc4eff31211807e7efb72cd0eae
Rasmus Mecklenburg <rasmus@familie-mecklenburg.de>
2019-05-01 22:06:28 +0200

    initial commit

---
+++ Graphics Test/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics_Test
+{
+    public partial class Form1 : Form
+    {
+        private System.Drawing.Bitmap myBitmap;
+        Scene Scene;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphicsObj = e.Graphics;
+
+            graphicsObj.DrawImage(myBitmap, 0, 0, myBitmap.Width, myBitmap.Height);
+
+            graphicsObj.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Graphics graphicsObj;
+            myBitmap = new Bitmap(ClientRectangle.Width,
+               ClientRectangle.Height,
+               System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            graphicsObj = Graphics.FromImage(myBitmap);
+            /*
+            Pen myPen = new Pen(Color.Plum, 3);
+            Rectangle rectangleObj = new Rectangle(0, 0, ClientRectangle.Width, ClientRectangle.Height);
+            graphicsObj.DrawEllipse(myPen, rectangleObj);
+            */
+            Scene = new Scene(ClientRectangle.Width, ClientRectangle.Height);
+            /*
+            Scene.AddSceneObject(new Point(new Vector3(0, 0, 0)));
+            Scene.AddSceneObject(new Point(new Vector3(0, 0, 100)));
+            Scene.AddSceneObject(new Point(new Vector3(0, 100, 0)));
+            Scene.AddSceneObject(new Point(new Vector3(0, 100, 100)));
+            Scene.AddSceneObject(new Point(new Vector3(100, 0, 0)));
+            Scene.AddSceneObject(new Point(new Vector3(100, 0, 100)));
+            Scene.AddSceneObject(new Point(new Vector3(100, 100, 0)));
+            Scene.AddSceneObject(new Point(new Vector3(100, 100, 100)));
+            */
+            
+            Scene.AddSceneObject(new Line(new Vector3(0, 0, 0), new Vector3(0, 0, 100)));
+            Scene.AddSceneObject(new Line(new Vector3(0, 0, 0), new Vector3(0, 100, 0)));
+            Scene.AddSceneObject(new Line(new Vector3(0, 0, 0), new Vector3(100, 0, 0)));
+            Scene.AddSceneObject(new Line(new Vector3(100, 100, 0), new Vector3(100, 100, 100)));
+            Scene.AddSceneObject(new Line(new Vector3(100, 100, 0), new Vector3(0, 100, 0)));
+            Scene.AddSceneObject(new Line(new Vector3(100, 100, 0), new Vector3(100, 0, 0)));
+
+            Scene.AddSceneObject(new Line(new Vector3(0, 100, 100), new Vector3(100, 100, 100)));
+            Scene.AddSceneObject(new Line(new Vector3(0, 100, 100), new Vector3(0, 0, 100)));
+            Scene.AddSceneObject(new Line(new Vector3(0, 100, 100), new Vector3(0, 100, 0)));
+            Scene.AddSceneObject(new Line(new Vector3(100, 0, 100), new Vector3(0, 0, 100)));
+            Scene.AddSceneObject(new Line(new Vector3(100, 0, 100), new Vector3(100, 100, 100)));
+            Scene.AddSceneObject(new Line(new Vector3(100, 0, 100), new Vector3(100, 0, 0)));
+            
+            
+            Scene.MoveScene(new Vector3(0, 0, 50));
+
+            Scene.DrawToGraphicsObj(graphicsObj);
+            graphicsObj.Dispose();
+
+            Console.WriteLine($"Screen Dimensions: {ClientRectangle.Width}, {ClientRectangle.Height}");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Keys pressedKey = e.KeyCode;
+
+            if (pressedKey == Keys.Right)
+            {
+                Scene.MoveScene(new Vector3(-1, 0, 0));
+            }
+            else if (pressedKey == Keys.Left)
+            {
+                Scene.MoveScene(new Vector3(1, 0, 0));
+            }
+            else if (pressedKey == Keys.Up)
+            {
+                Scene.MoveScene(new Vector3(0, 1, 0));
+            }
+            else if (pressedKey == Keys.Down)
+            {
+                Scene.MoveScene(new Vector3(0, -1, 0));
+            }
+            else if (pressedKey == Keys.Add)
+            {
+                Scene.MoveScene(new Vector3(0, 0, 1));
+            }
+            else if (pressedKey == Keys.Subtract)
+            {
+                Scene.MoveScene(new Vector3(0, 0, -1));
+            }
+
+
+            UpdateImage();
+        }
+
+        public void UpdateImage()
+        {
+            Graphics graphicsObj = Graphics.FromImage(myBitmap);
+
+            graphicsObj.Clear(Color.Black);
+
+            Scene.DrawToGraphicsObj(graphicsObj);
+
+            Refresh();
+        }
+    }
+}
---
+++ Graphics Test/Lib.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Graphics_Test
+{
+    class Lib
+    {
+
+    }
+
+    class Camera
+    {
+        //Postition of the camera
+        Vector3 position;
+        //Rotation of the camera
+        Vector3 rotation;
+        //Position of the image plane relative to the camera
+        Vector3 ImagePlanePos;
+
+        int fov;
+
+        int width, height;
+
+        Brush brush;
+        Pen pen;
+
+        public Camera(double Width, double Height)
+        {
+            position = new Vector3();
+            rotation = new Vector3();
+            ImagePlanePos = new Vector3(Width/2,Height/2,140);
+            brush = Brushes.Green;
+            pen = new Pen(Color.Green);
+        }
+
+        public Vector2 LocalSpace2ScreenSpace(Vector3 relativePosition)
+        {
+            double outX, outY;
+
+            outX = ImagePlanePos.Z * relativePosition.X / relativePosition.Z + ImagePlanePos.X;
+
+            outY = ImagePlanePos.Z * relativePosition.Y / relativePosition.Z + ImagePlanePos.Y;
+
+            return new Vector2(outX, outY);
+        }
+
+        public void DrawSceneObject(SceneObject sceneObj, Graphics graphicsObj)
+        {
+            if (sceneObj is Point)
+            {
+                Vector2 pos = LocalSpace2ScreenSpace(((Point)sceneObj).position);
+                graphicsObj.FillRectangle(brush, (float)pos.X, (float)pos.Y, 3, 3);
+            }
+            else if (sceneObj is Line)
+            {
+                Vector2 pos1 = LocalSpace2ScreenSpace(((Line)sceneObj).point1);
+                Vector2 pos2 = LocalSpace2ScreenSpace(((Line)sceneObj).point2);
+                graphicsObj.DrawLine(pen, (float)pos1.X, (float)pos1.Y, (float)pos2.X, (float)pos2.Y);
+            }
+        }
+    }
+
+    struct Vector2
+    {
+        public double X, Y;
+
+        public Vector2(double _x, double _y)
+        {
+            X = _x;
+            Y = _y;
+        }
+
+        public string Repr()
+        {
+            return $"({X}, {Y})";
+        }
+
+        public static Vector2 operator + (Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.X + b.X, a.Y + b.Y);
+        }
+    }
+
+    struct Vector3
+    {
+        public double X, Y, Z;
+
+        public Vector3(double _x, double _y, double _z)
+        {
+            X = _x;
+            Y = _y;
+            Z = _z;
+        }
+
+        public string Repr()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+
+        public static Vector3 operator + (Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X+b.X, a.Y+b.Y, a.Z+b.Z);
+        }
+    }
+
+    class Scene
+    {
+        Camera MainCamera;
+        List<SceneObject> sceneObjects;
+
+        Vector2 Dimensions;
+
+        public Scene(int Width, int Height)
+        {
+            sceneObjects = new List<SceneObject>();
+            MainCamera = new Camera(Width, Height);
+            Dimensions = new Vector2(Width, Height);
+        }
+
+        public void AddSceneObject(SceneObject sceneObject)
+        {
+            sceneObjects.Add(sceneObject);
+        }
+
+        public void DrawToGraphicsObj(Graphics graphicsObj)
+        {
+            foreach (SceneObject target in sceneObjects)
+            {
+                MainCamera.DrawSceneObject(target, graphicsObj);
+            }
+        }
+
+        public void MoveScene(Vector3 direction)
+        {
+            foreach (SceneObject item in sceneObjects)
+            {
+                item.Move(direction);
+            }
+        }
+    }
+
+    abstract class SceneObject
+    {
+        public abstract void Move(Vector3 direction);
+    }
+
+    class Point : SceneObject
+    {
+        public Vector3 position;
+        public Point(Vector3 _position)
+        {
+            position = _position;
+        }
+
+        public override void Move(Vector3 direction)
+        {
+            position += direction;
+        }
+    }
+
+    class Line : SceneObject
+    {
+        public Vector3 point1, point2;
+        public Line(Vector3 _point1, Vector3 _point2)
+        {
+            point1 = _point1;
+            point2 = _point2;
+        }
+
+        public override void Move(Vector3 direction)
+        {
+            point1 += direction;
+            point2 += direction;
+        }
+    }
+}
